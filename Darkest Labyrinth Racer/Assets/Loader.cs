f247a55a8ddd5c08fabe95dc2f59b8776cf6d0b6
Stefan Insam <ramsesoriginal@gmail.com>
2015-06-27 15:37:23 +0200

    Created Project

---
+++ Darkest Labyrinth Racer/Assets/Loader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Cancel"))
+			ExitGame ();
+	}
+
+	public void LoadGame() {
+		Application.LoadLevel("Game");
+	}
+	
+	public void ExitGame() {
+		if (Application.loadedLevelName == "Main") {
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit ();
+#endif
+		} else {
+			Application.LoadLevel ("Main");
+		}
+	}
+
+}
