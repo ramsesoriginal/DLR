d5f2ed2dc17125dadfad2952acf989541db58137
Stefan Insam <ramsesoriginal@gmail.com>
2015-06-27 19:23:13 +0200

    Closes #5

---
+++ Darkest Labyrinth Racer/Assets/TimeSinceLevelStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeSinceLevelStart : MonoBehaviour {
+
+	public UnityEngine.UI.Text timer;
+	public GameObject winPanel;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!winPanel.activeSelf)
+			timer.text = Time.timeSinceLevelLoad.ToString ("F3");
+	}
+}
